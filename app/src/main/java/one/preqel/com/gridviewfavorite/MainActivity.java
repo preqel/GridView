023c9b7cb8eab10f862f5bb641b03d8eb335d05c
preqel <358795715@qq.com>
2017-10-03 14:14:51 +0800

    修改manifest.xml配置文件

--- app/src/main/java/one/preqel/com/gridviewfavorite/MainActivity.java
+++ app/src/main/java/one/preqel/com/gridviewfavorite/MainActivity.java
@@ -9,7 +9,8 @@ import gridview.preqel.com.gridviewlibrary.CustomUtil;
 /*
     入口类，demo演示了可滑动的gridview，并且可以配置图片和点击事件，只需要修改menu.xml里面的内容即可
     底层的原理，就是利用java反射技术把xml里面的东西反射到对应的点击事件
-     作者：wangkang
+    作者：wangkang
+    时间：20171003
  */
 public class MainActivity extends AppCompatActivity {
 
